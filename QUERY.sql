7f734e04961e7635dd70b0178f1c718827513a1a
Habibur Rahman Rahel <rahel.ahmedr@gmail.com>
2026-06-14 23:42:47 +0600

    Query 1 to 4

--- QUERY.sql
+++ QUERY.sql
@@ -98,3 +98,31 @@ INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status
 (503, 2, 101, 'A-13', 'Confirmed', 150.00),
 (504, 2, 101, NULL, NULL, 150.00),
 (505, 3, 102, 'C-20', 'Pending', 120.00);
+
+-- =========================================================================
+-- PART 2: (SOLUTION)
+-- =========================================================================
+
+-- Query 1: Retrieve all upcoming football matches belonging to the 'Champions League' where the match status is 'Available'.
+SELECT match_id, fixture, base_ticket_price 
+FROM Matches 
+WHERE tournament_category = 'Champions League' 
+  AND match_status = 'Available';
+
+-- Query 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive).
+SELECT user_id, full_name, email 
+FROM Users 
+WHERE full_name ILIKE 'Tanvir%' 
+   OR full_name ILIKE '%Haque%';
+
+-- Query 3: Retrieve all booking records where the payment status is missing (NULL), replacing the empty result with 'Action Required'.
+SELECT booking_id, user_id, match_id, 
+       COALESCE(payment_status, 'Action Required') AS systematic_status 
+FROM Bookings 
+WHERE payment_status IS NULL;
+
+-- Query 4: Retrieve match booking details along with the User's full name and the scheduled Match fixture teams.
+SELECT b.booking_id, u.full_name, m.fixture, b.total_cost 
+FROM Bookings b
+INNER JOIN Users u ON b.user_id = u.user_id
+INNER JOIN Matches m ON b.match_id = m.match_id;
